18ec100e37fcd7d2fa356d531b6c9d0dc561a378
dairon20jb <dairon.barbosa@javeriana.edu.co>
2020-11-02 12:17:06 -0500

    Avance

---
+++ EntrenarModelo.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+import numpy as np
+dataPath = 'C:\PRUEBA\Data'#Cambia a la ruta donde hayas almacenado Data
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+labels = []
+facesData = []
+label = 0
+for nameDir in peopleList:
+    personPath = dataPath + '/' + nameDir
+    print('Leyendo las imágenes')
+    for fileName in os.listdir(personPath):
+        print('Rostros: ', nameDir + '/' + fileName)
+        labels.append(label)
+        facesData.append(cv2.imread(personPath+'/'+fileName,0))
+        #image = cv2.imread(personPath+'/'+fileName,0)
+        #cv2.imshow('image',image)
+        #cv2.waitKey(10)
+    label = label + 1
+print('labels= ',labels)
+print('Número de etiquetas 0: ',np.count_nonzero(np.array(labels)==0))
+print('Número de etiquetas 1: ',np.count_nonzero(np.array(labels)==1))
+print('Número de etiquetas 2: ',np.count_nonzero(np.array(labels)==2))
+print('Número de etiquetas 3: ',np.count_nonzero(np.array(labels)==3))
+print('Número de etiquetas 4: ',np.count_nonzero(np.array(labels)==4))
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
+# Entrenando el reconocedor de rostros
+print("Entrenando...")
+face_recognizer.train(facesData, np.array(labels))
+face_recognizer.write('modeloEigenFace.xml')
+#face_recognizer.write('modeloFisherFace.xml')
+#face_recognizer.write('modeloLBPHFace.xml')
+print("Modelo almacenado...")
